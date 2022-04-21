970ae87b4d2c527bf323c8594bf32fad3a8daeef
Stoian Ivanov <st.iv.sdr@gmail.com>
2022-04-21 16:36:54 +0300

    wip:ui start; copy some  code from node

---
+++ http-digest-auth/browser/src/shelly_auth_ui.ts
@@ -0,0 +1,107 @@
+import {HexHash,JRPCPost_t,setlog,shellyHttpCall} from './shelly_http_call.js'
+
+const mainui={
+	host:<HTMLInputElement><any>null,
+	port:<HTMLInputElement><any>null,
+	password:<HTMLInputElement><any>null,
+	method:<HTMLInputElement><any>null,
+	params:<HTMLInputElement><any>null,
+	log:<HTMLTextAreaElement><any>null,
+}
+
+function ls_persist(){
+	localStorage.setItem('host',mainui.host.value);
+	localStorage.setItem('port',mainui.port.value);
+	localStorage.setItem('password',mainui.password.value); //don't do this in non-testing scenarios 
+	localStorage.setItem('method',mainui.method.value);
+	localStorage.setItem('params',mainui.params.value);
+}
+function ls_getItem_def(key:string,def:string):string{
+	const res=localStorage.getItem(key);
+	if (res!==null) return res;
+	return def;
+}
+
+export function addlog(msg:string){
+	mainui.log.innerText+="\r\n"+msg;
+}
+setlog(addlog);
+
+function fill_ui_holder (holder:Record<string,HTMLElement>,selector:string,src:Element=document.body) {
+	const elems=src.querySelectorAll(selector);
+	for (let i=0; i<elems.length; i++) {
+		const e=elems.item(i);
+		if (! (e instanceof HTMLElement)) continue;
+
+		let bindname=e.getAttribute('data-bind');
+		if (bindname===null){
+			bindname=e.getAttribute('name');
+			if (bindname===null){
+				bindname=e.getAttribute('id');
+			}
+		}
+		if (bindname===null) continue;
+		if (holder.hasOwnProperty(bindname)) holder[bindname]=e;
+	}
+	let failed_binds:string[]=[];
+	for (let k in  holder) if (holder[k]==null) failed_binds.push(k)
+	if (failed_binds.length>0)throw new Error('Binds failed with selector '+selector+' missing elements:'+failed_binds.join(','));
+}
+
+export function init (){
+	fill_ui_holder(mainui,'.js_mainui');
+	const h=HexHash('1234567890');
+	if (h==='c775e7b757ede630cd0aa1113bd102661ab38829ca52a6422ab782862f268646') {
+		addlog("sha256 seems to work OK on this browser .. good!");
+	} else {
+		addlog("sha256 seems to be BROKEN on this browser!");
+		return;
+	}
+	mainui.host.value=ls_getItem_def('host','');
+	mainui.port.value=ls_getItem_def('port','80');
+	mainui.password.value=ls_getItem_def('password',''); //don't do this in non-testing scenarios
+	mainui.method.value=ls_getItem_def('method','Shelly.GetStatus');
+	mainui.params.value=ls_getItem_def('params','');
+}
+
+export function clearlog() {
+	mainui.log.innerText='';
+}
+
+export function call_jrpc() {
+	let postData:JRPCPost_t={
+		id:0,
+		method:mainui.method.value
+	}
+
+	const port=Number(mainui.port.value);
+	if (!(port>0 && port<65536)) {
+		addlog("Port should be in range 1 to 65535 inclusively!");
+
+	}
+	if (mainui.params.value!='') {
+		try {
+			postData.params = JSON.parse(mainui.params.value);
+		} catch (err) {
+			addlog("Params fail to parse. This MUST be strinct JSON. Check if you need to wrap the params in ' ");
+			return;
+		};
+	}
+	ls_persist();
+
+	shellyHttpCall(postData, mainui.host.value, port, mainui.password.value).then((data) => {
+		if (postData.auth) {
+			addlog("Device response post auth: ");
+		} else {
+			addlog("Device response pre auth: ");
+		}
+		try {
+			addlog(JSON.stringify(JSON.parse(data), null, 2));
+		} catch (e) {
+			addlog("failed to parse the responce from the device!");
+		}
+	}).catch((err) => {
+		addlog("Request failed :"+ String(err));
+	});
+}
+
---
+++ http-digest-auth/browser/src/shelly_http_call.ts
@@ -0,0 +1,187 @@
+import {RawSha256} from './RawSha256.js';
+
+const hexab = '0123456789abcdef';
+//const hexab = '0123456789ABCDEF';
+
+
+export interface authParams_t {
+	username?: string;
+	nonce: string;
+	cnonce?: string;
+	realm: string;
+	algorithm: string;
+	response?: string;
+}
+
+
+export interface JRPCPost_t {
+	id: number,
+	method: string;
+	params?: Record<string, unknown>;
+	auth?: authParams_t;
+}
+export type log_f=(msg:string)=>void;
+
+let log:log_f;
+
+export function setlog(logfn:log_f) {
+	log=logfn
+}
+
+export function HexHash(s:string):string {
+	const h=new RawSha256();
+	h.update(new TextEncoder().encode(s));
+	let res='';
+	const digb=h.digest();
+	for (let b of digb) {
+		res+= hexab[b >> 4] + hexab[b & 15];;
+	}
+	return res;
+}
+
+export const shellyHttpHashAlgo = "sha256";
+
+
+const static_noise_sha256 = ':auth:6370ec69915103833b5222b368555393393f098bfbfbb59f47e0590af135f062'; // = ':auth:'+HexHash("dummy_method:dummy_uri");
+
+
+export function isauthParams(p: any): p is authParams_t {
+	return (
+		p && typeof (p) == 'object'
+		&& typeof (p.nonce) == 'string'
+		&& typeof (p.realm) == 'string'
+		&& typeof (p.algorithm) == 'string'
+	);
+}
+
+export function complementAuthParams(authParams: authParams_t, username: string, password: string) {
+	console.log("complementAuthParams initial params: "+JSON.stringify(authParams))
+	authParams.username = username;
+	authParams.nonce = String(parseInt(authParams.nonce, 16));
+	authParams.cnonce = String(Math.floor(Math.random() * 10e8));
+
+	let resp = HexHash(username + ":" + authParams.realm + ":" + password);
+	console.log("complementAuthParams resp start:"+resp);
+	resp += ":" + authParams.nonce;
+	resp += ":1:" + authParams.cnonce + static_noise_sha256;
+
+	authParams.response = HexHash(resp);
+	console.log("complementAuthParams final params: "+JSON.stringify(authParams))
+
+};
+
+
+export const shellyHttpUsername = "admin"; // always
+
+
+const match_dquote_re = /^\"|\"$/g;
+const match_coma_space_re = /,? /;
+  
+//throws error on unexpected algos or missing/invalid header parts!
+export function extractAuthParams(authHeader: string): authParams_t {
+
+	let [authType, ...auth_parts] = authHeader.trim().split(match_coma_space_re);
+
+	if (authType.toLocaleLowerCase() != 'digest') {
+	  throw new Error("WWW-Authenticate header is requesting unusial auth type " + authType + "instead of Digest");
+	}
+
+	let authParams: Record<string, string> = {};
+	for (let part of auth_parts) {
+		let [_key, _value] = part.split("=");
+		_value = _value.replace(match_dquote_re, '');
+
+		if (_key == 'algorithm' && _value != 'SHA-256') {
+			throw new Error("WWW-Authenticate header is requesting unusial algorithm:" + _value + " instead of SHA-256");
+		}
+
+		if (_key == 'qop') {
+			if (_value != 'auth') {
+				throw new Error("WWW-Authenticate header is requesting unusial qop:" + _value + " instead of auth");
+			}
+			continue;
+		}
+
+		authParams[_key.trim()] = _value.trim();
+	}
+
+	if (!isauthParams(authParams)) {
+		throw new Error("invalid WWW-Authenticate header from device?!");
+	}
+	return authParams;
+}
+  
+export function shellyHttpCall(postdata: JRPCPost_t, host: string, port:number, password: string): Promise<string> {
+	return new Promise((resolve, reject) => {
+
+		const options: http.RequestOptions = {
+			hostname: host,
+			port: port,
+			path: "/rpc",
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			timeout: 500
+		};
+
+		const req = http.request(options, async (response) => {
+			let buffer = Buffer.alloc(0);
+
+			if (response.statusCode == 401) {
+				// Not authenticated
+				if (password=='') {
+					return reject(new Error("Failed to authenticate!"));
+				}
+				//look up the challenge header
+				let authHeader = response.headers["www-authenticate"];
+				if (authHeader == undefined) {
+					return reject(new Error("WWW-Authenticate header is missing in the response?!"));
+				}
+				try {
+					const authParams = extractAuthParams(authHeader);
+					complementAuthParams(authParams, shellyHttpUsername, password);
+					//Retry with challenge response object
+					postdata.auth = authParams;
+					return resolve(await shellyHttpCall(postdata, host,port, ''));
+				} catch (e) {
+					if (!(e instanceof Error)) e = new Error(String(e));
+					return reject(e);
+				}
+			}
+
+			//Authenticated, or no auth needed! fetch data:
+
+			response.on("error", (error) => {
+				reject(error);
+			});
+
+			response.on("timeout", () => {
+				reject(new Error("Timeout"));
+			});
+
+			response.on("data", (chunk) => {
+				buffer = Buffer.concat([buffer, chunk]);
+			});
+
+			response.on("end", () => {
+				resolve(buffer.toString("utf8"));
+			});
+
+		});
+
+		//setup request error listeners, write post data, indicate write end:
+
+		req.on("timeout", () => {
+		req.destroy();
+		reject(new Error("Timeout"));
+		});
+
+		req.on("error", (error: unknown) => {
+		reject(new Error("Request error:" + String(error)));
+		})
+
+		req.write(JSON.stringify(postdata));
+		req.end();
+	});
+};
